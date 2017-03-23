0bfc390135175610dd65493bb89d3c0d5c207184
arulselvam <arul.selvam@pearldatadirect.com>
2017-03-24 03:53:18 +0530

    masterpush

---
+++ src/pages/page1/page1.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+import { ModalController } from 'ionic-angular';
+import { Page3 } from '../page3/page3';
+import { Page2 } from '../page2/page2';
+
+@Component({
+  selector: 'page-page1',
+  templateUrl: 'page1.html'
+})
+export class Page1 {
+
+  constructor(public navCtrl: NavController,public modalCtrl: ModalController) {
+    
+  }
+  createacc() {
+    console.log('customer register page');
+    this.navCtrl.push(Page3);
+  }
+  gotodash() {
+    console.log('Dashboard page');
+    this.navCtrl.push(Page2);
+  }
+  presentModal() {
+    let modal = this.modalCtrl.create(Page3);
+    modal.present();
+  }
+
+}
---
+++ src/pages/page2/page2.ts
@@ -0,0 +1,156 @@
+import { Component, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-page2',
+  templateUrl: 'page2.html'
+})
+export class Page2 {
+  @ViewChild('barCanvas') barCanvas;
+    @ViewChild('doughnutCanvas') doughnutCanvas;
+    @ViewChild('lineCanvas') lineCanvas;
+ 
+    barChart: any;
+    doughnutChart: any;
+    lineChart: any;
+  selectedItem: any;
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
+
+    // Let's populate this page with some filler content for funzies
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
+
+    this.items = [];
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        note: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }
+  }
+
+  itemTapped(event, item) {
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(Page2, {
+      item: item
+    });
+  }
+
+  ionViewDidLoad() {
+ 
+        this.barChart = new Chart(this.barCanvas.nativeElement, {
+ 
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+ 
+        });
+ 
+        this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+ 
+            type: 'doughnut',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56",
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ]
+                }]
+            }
+ 
+        });
+ 
+        this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+ 
+            type: 'line',
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [
+                    {
+                        label: "My First dataset",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [65, 59, 80, 81, 56, 55, 40],
+                        spanGaps: false,
+                    }
+                ]
+            }
+ 
+        });
+ 
+    }
+ 
+
+
+
+
+}
